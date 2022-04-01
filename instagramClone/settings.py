9dfa559a23f089ec3406ed235dc06604441eb7c8
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-01 18:03:24 +0300

    connected to postgress database instagramclone

--- instagramClone/settings.py
+++ instagramClone/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'instagramClone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagramclone',
+        'USER': 'elijah',
+        'PASSWORD': 'eliki13720'
     }
 }
 
