2e483fde6a7131eb44357ed018ee68f9a6e2e919
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-01 18:31:53 +0300

    created profile class

--- myInstagram/models.py
+++ myInstagram/models.py
@@ -13,7 +13,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     image_name = models.CharField(max_length =60)
     image_caption= models.CharField(max_length=100)
-    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     likes = models.CharField(max_length=10)
     comments = models.CharField(max_length=50)
 
