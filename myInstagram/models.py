3d3895172805361c338507fd86d1b9623b8f9af6
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-01 18:10:35 +0300

    created image model

--- myInstagram/models.py
+++ myInstagram/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    image_name = models.CharField(max_length =60)
+    image_caption= models.CharField(max_length=100)
+    image_profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    likes = models.CharField(max_length=10)
+    comments = models.CharField(max_length=50)
