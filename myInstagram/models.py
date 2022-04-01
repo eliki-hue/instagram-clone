0a862d4b5ce2082a428c4429883e7e3e108c217f
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-01 18:15:11 +0300

    imported django user model

--- myInstagram/models.py
+++ myInstagram/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
 # Create your models here.
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     image_name = models.CharField(max_length =60)
     image_caption= models.CharField(max_length=100)
-    image_profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.CharField(max_length=10)
     comments = models.CharField(max_length=50)
