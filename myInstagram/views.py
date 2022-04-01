06ae14cb615ead6aaffdc53136ef5f2274e529d8
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-01 21:38:40 +0300

    added a view and index.html

--- myInstagram/views.py
+++ myInstagram/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Image, Profile
 
 # Create your views here.
+
+def welcome(request):
+    images = Image.objects.all()
+
+    return render('index.html',{'images':images})
+
