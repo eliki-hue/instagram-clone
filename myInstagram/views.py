0441059b807ddbab606e64cfdff22b5a42603ea5
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-02 16:26:06 +0300

    installed django-registration

--- myInstagram/views.py
+++ myInstagram/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Image, Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='/accounts/login/')
 def welcome(request):
     images = Image.objects.all()
     message ="Welcome to my Instagram clone"
