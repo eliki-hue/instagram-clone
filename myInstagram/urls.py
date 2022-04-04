f98fe1024268bb587d43f054ff19fc04fc9c271e
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-04 16:06:06 +0300

    added profile form

--- myInstagram/urls.py
+++ myInstagram/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     
     path('first_page',views.first_page, name='login_page'),
     path('', views.home, name ='home'),
-    path('my_profile', views.profile_display, name='profile_display')
+    path('my_profile', views.profile_display, name='profile_display'),
+    path('update_profile', views.profile_update, name='profile_update'),
+    
     # path('accounts/register/', views.register, name='register'),
     
 
--- myInstagram/views.py
+++ myInstagram/views.py
@@ -2,7 +2,7 @@ from email import message
 from django.shortcuts import redirect, render
 from .models import Image, Profile
 from django.contrib.auth.decorators import login_required
-from .form import ProfileForm
+from .forms import ProfileForm
 
 # Create your views here.
 
@@ -18,21 +18,28 @@ def home(request):
 
     return render(request,'index.html',{'images':images, 'message': message})
 def profile_update(request):
-    form =ProfileForm(request.POST)
-    if form.is_valid():
-        form.save()
-        profile= Profile.objects.get().all()
-        return render(request, 'profile.html',{'profile':profile})
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            username = current_user
+            useremail=form.cleaned_data['useremail']
+            userage=form.cleaned_data['userage']
+            profile_image=form.cleaned_data['profile_image']
+            user_password=form.cleaned_data['user_password']
+    else:
+        form = ProfileForm()
+    return render(request, 'profiledisplay.html',{'form':form})
+       
+       
 
 def profile_display(request):
 
-    try:
-        profile= Profile.objects.get().all()
-    except:
-        message= "no profile information"
-        return render(request, 'profile.html',{'profile':'none', 'message':message})
+    
+    # profile= Profile.objects.get().all()
+   
 
-    return render(request, 'profile.html',{'profile':profile})
+    return render(request, 'profile.html')
 
 
     
