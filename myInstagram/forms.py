ea3f0ac60095e57283ff170eff8d8a9e391c51b6
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-04 22:17:57 +0300

    added codes to update profile

--- myInstagram/forms.py
+++ myInstagram/forms.py
@@ -4,4 +4,5 @@ from .models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
+        exclude=['username']
         fields ='__all__'
--- myInstagram/views.py
+++ myInstagram/views.py
@@ -22,25 +22,39 @@ def profile_update(request):
     if request.method == 'POST':
         form = ProfileForm(request.POST, request.FILES)
         if form.is_valid():
-            # username = form.cleaned_data['username']
-            # useremail=form.cleaned_data['useremail']
-            # userage=form.cleaned_data['userage']
-            # profile_image=form.cleaned_data['profile_image']
-            # user_password=form.cleaned_data['user_password']
-            # info=Profile(username,useremail,userage,profile_image,user_password)
-            form.save()
+            profile= Profile.objects.filter(username=current_user)
+            print(profile)
+            if profile:
+                print('profile exist')
+                username = current_user
+                useremail=form.cleaned_data['useremail']
+               
+                userage=form.cleaned_data['userage']
+                profile_image=form.cleaned_data['profile_image']
+                user_password=form.cleaned_data['user_password']
+                Profile.objects.filter(username=current_user).update(useremail=useremail, userage=userage,profile_image=profile_image,user_password=user_password)
+            else:
+                print('profile does not exist')
+                profile=form.save(commit=False)
+                profile.username= current_user
+                profile.create()
+
             message='saved successfuly'
+            # profile_display(request)
+            return redirect(profile_display)
+    
+            
     else:
         form = ProfileForm()
-        message ='not saved'
-    return render(request, 'profiledisplay.html',{'form':form, 'message':message})
+        
+    return render(request, 'profiledisplay.html',{'form':form})
        
        
 
 def profile_display(request):
 
     current_user = request.user
-    profile= Profile.objects.filter('username'==current_user)
+    profile= Profile.objects.filter(username=current_user)
    
 
     return render(request, 'profile.html',{'profile':profile})
